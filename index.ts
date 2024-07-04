89caadeeefe041c72813c696a95d3c706205bfa1
hassanmasood200716 <hassanmasood9876@gmail.com>
2024-07-05 02:14:21 +0500

    push my game to github

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+console.log("the developer of this game is hassan masood")
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([
+  {
+    name: "userguessednumber",
+    type: "number",
+    message: "plz guess a number between 1 till 10: ",
+  },
+]);
+
+if (answer.userguessednumber === randomNumber) {
+  console.log("congratulation you guessed write number");
+} else {
+  console.log("you guessed wrong number");
+}
